b3fc5084587ea780bb6670f40fa8abc7fb46d1b0
ALAMI <ilias.alami.13110@gmail.com>
2025-10-16 12:18:34 +0200

    modif vpc terraform

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,5 +3,17 @@ terraform {
     bucket = "tf-state-404infra"
     key    = "state"
     region = "eu-west-3"
+    profile = "Florian-dev"
   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+  profile = "Florian-dev"
 }
