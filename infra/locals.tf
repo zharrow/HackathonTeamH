77f16ada9d92a888787ee468cabdaffa7d581ad1
IliasAlami <95253959+IliasAlami@users.noreply.github.com>
2025-10-17 14:33:43 +0200

    Update locals.tf

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,4 +1,7 @@
 locals {
-  container_name = "nginx"
-  container_port = "80"
+  container_name = "web"
+  container_port = "3000"
+  name_as_sg = "404sg"
+  name_alb = "404LB"
+  domain_name = "aws.corentinptrl.dev"
 }
